f498104a02ce0930e00f1927838a41d97bb468a1
Otávio Augusto Chrispim De Paiva <otaviochrispim@gmail.com>
2025-10-04 15:39:42 -0300

    update project

--- hospital-exams/src/components/AuthGuard/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet, useLocation } from 'react-router-dom';
-import { useAuth } from '../../service/auth';
-
-export default function AuthGuard() {
-  const isAuthed = useAuth((s) => s.isAuthenticated());
-  const loc = useLocation();
-  if (!isAuthed) return <Navigate to="/login" replace state={{ from: loc }} />;
-  return <Outlet />;
-}
